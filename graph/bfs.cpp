5b6b9f207f9a33e33c874086afa11c2cb0e1df22
nobdefender <v1234198@gmail.com>
2022-09-30 22:10:03 +0300

    add bfs

--- graph/bfs.cpp
+++ graph/bfs.cpp
@@ -1,19 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<vector<int>> v;
+vector<int> u;
 queue<int> q;
-q.push (s);
-vector<bool> used (n);
-vector<int> d (n), p (n);
-used[s] = true;
-p[s] = -1;
-while (!q.empty()) {
-	int v = q.front();
-	q.pop();
-	for (size_t i=0; i<g[v].size(); ++i) {
-		int to = g[v][i];
-		if (!used[to]) {
-			used[to] = true;
-			q.push (to);
-			d[to] = d[v] + 1;
-			p[to] = v;
-		}
-	}
+
+void bfs(int i, int n) {
+  q.push(i); u[i] = 1;
+  while(!q.empty()) {
+    int j = q.front(); q.pop();
+    for (auto &x: v[j])
+      if (!u[x]) {
+        u[x] = 1;
+        q.push(x);
+      }
+    cout << j << ' ';
+  }
+}
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  v.resize(n);
+  for (int i = 0; i < m; i++) {
+    int x, y;
+    cin >> x >> y;
+    v[--x].push_back(--y);
+    v[y].push_back(x);
+  }
+  for (int i = 0; i < n; i++) {
+      u.assign(n ,0);
+      bfs(i, n);
+      cout << '\n';
+    }
 }
