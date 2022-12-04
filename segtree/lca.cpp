2237ce14180748a471a2d3fbe41748197c721f5c
nobdefender <v1234198@gmail.com>
2022-12-04 18:49:36 +0300

    add lca_segtree and prefix_function

---
+++ segtree/lca.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+#include<map>
+#include<string>
+#include<cmath>
+#include<stack>
+#include<iomanip>
+ 
+#define int long long
+#define all(x) (x).begin(), (x).end()
+#define endl "\n"
+ 
+using namespace std;
+ 
+vector<vector<pair<int, int>>> g;
+vector<bool> used;
+typedef vector<int>::const_iterator const_graph_iter;
+vector<int> lca_h, lca_dfs_list, lca_first, lca_tree;
+vector<char> lca_dfs_used;
+ 
+void lca_dfs (int v, int h = 1)
+{
+	lca_dfs_used[v] = true;
+	lca_h[v] = h;
+	lca_dfs_list.push_back (v);
+	for (int i = 0; i < g[v].size(); i++)
+		if (!lca_dfs_used[g[v][i].first])
+		{
+			lca_dfs (g[v][i].first, h+1);
+			lca_dfs_list.push_back (v);
+		}
+}
+ 
+void lca_build_tree (int i, int l, int r)
+{
+	if (l == r)
+		lca_tree[i] = lca_dfs_list[l];
+	else
+	{
+		int m = (l + r) >> 1;
+		lca_build_tree (i+i, l, m);
+		lca_build_tree (i+i+1, m+1, r);
+		if (lca_h[lca_tree[i+i]] < lca_h[lca_tree[i+i+1]])
+			lca_tree[i] = lca_tree[i+i];
+		else
+			lca_tree[i] = lca_tree[i+i+1];
+	}
+}
+ 
+void lca_prepare (int root)
+{
+	int n = (int) g.size();
+	lca_h.resize (n);
+	lca_dfs_list.reserve (n*2);
+	lca_dfs_used.assign (n, 0);
+ 
+	lca_dfs (root);
+ 
+	int m = (int) lca_dfs_list.size();
+	lca_tree.assign (lca_dfs_list.size() * 4 + 1, -1);
+	lca_build_tree (1, 0, m-1);
+ 
+	lca_first.assign (n, -1);
+	for (int i = 0; i < m; ++i)
+	{
+		int v = lca_dfs_list[i];
+		if (lca_first[v] == -1)
+			lca_first[v] = i;
+	}
+}
+ 
+int lca_tree_min (int i, int sl, int sr, int l, int r)
+{
+	if (sl == l && sr == r)
+		return lca_tree[i];
+	int sm = (sl + sr) >> 1;
+	if (r <= sm)
+		return lca_tree_min (i+i, sl, sm, l, r);
+	if (l > sm)
+		return lca_tree_min (i+i+1, sm+1, sr, l, r);
+	int ans1 = lca_tree_min (i+i, sl, sm, l, sm);
+	int ans2 = lca_tree_min (i+i+1, sm+1, sr, sm+1, r);
+	return lca_h[ans1] < lca_h[ans2] ? ans1 : ans2;
+}
+ 
+int lca (int a, int b)
+{
+	int left = lca_first[a],
+		right = lca_first[b];
+	if (left > right)  swap (left, right);
+	return lca_tree_min (1, 0, (int)lca_dfs_list.size()-1, left, right);
+}
+ 
+signed main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+ 
+    long long _t = 1;
+
+    while (_t--) {
+        int n = 0;
+        cin >> n;
+        g.resize(n);
+        used.resize(n);
+        // чтение граффа и тд
+        lca_prepare(0); 
+    }
+ 
+    return 0;
+}
